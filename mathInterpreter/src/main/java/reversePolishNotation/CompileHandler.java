51ba1acd67f37831f172ff63d6f6b631b96cd491
zhengwei <zhengweiSE@gmail.com>
2015-08-24 15:39:05 +0800

    改进测试方法，没了

--- mathInterpreter/src/main/java/reversePolishNotation/CompileHandler.java
+++ mathInterpreter/src/main/java/reversePolishNotation/CompileHandler.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-import behavioral_patterns.interpreter.src.PostfixStringHandler;
-
 public class CompileHandler {
 	private List<String> numberStack = new ArrayList<String>();
 	private Stack<String> operatorsSatck = new Stack<String>();
@@ -130,29 +128,4 @@ public class CompileHandler {
 			this.postfix.append(s);
 		this.numberStack.clear();
 	}
-
-	public static void main(String[] args) {
-		String[] testStrings = { "(2+3)4", "(1+2)", "3(1+2)", "2*-3", "2**-3", "-(22-10)", "1.22+3", "3.33*4.12/5.22", "3.1(2/3.4)", "3.1*2/3.4", "2*3+4", "-2(3+4)", "-2*-3", "-(22-10*(1-1)-3+-2)",
-				"1+++2", "1----2", "4/2", "4-3" };
-		for (String s : testStrings) {
-			System.out.println("-----------------------------");
-			System.out.println("测试字符：" + s);
-			CompileHandler handler = new CompileHandler(s);
-			String postFix = null;
-			try {
-				postFix = handler.handl();
-			} catch (Exception e) {
-				System.out.println("后缀式解析出错，退出");
-				e.printStackTrace();
-			}
-			System.out.println("后缀式：" + postFix);
-			try {
-				System.out.println("计算结果：" + PostfixStringHandler.handler(postFix));
-
-			} catch (Exception e) {
-				System.out.println("计算出错，退出");
-			}
-		}
-	}
-
 }
--- mathInterpreter/src/test/java/TestMain.java
+++ mathInterpreter/src/test/java/TestMain.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import reversePolishNotation.CompileHandler;
 import behavioral_patterns.interpreter.src.PostfixStringHandler;
 
 public class TestMain {
@@ -10,4 +11,26 @@ public class TestMain {
 		System.out.println(re);
 	}
 
+	@Test
+	public void test2() {
+		String[] testStrings = { "(2+3)4", "(1+2)", "3(1+2)", "2*-3", "2**-3", "-(22-10)", "1.22+3", "3.33*4.12/5.22", "3.1(2/3.4)", "3.1*2/3.4", "2*3+4", "-2(3+4)", "-2*-3", "-(22-10*(1-1)-3+-2)",
+				"1+++2", "1----2", "4/2", "4-3" };
+		for (String s : testStrings) {
+			System.out.println("-----------------------------");
+			System.out.println("测试字符：" + s);
+			CompileHandler handler = new CompileHandler(s);
+			String postFix = null;
+			try {
+				postFix = handler.handl();
+			} catch (Exception e) {
+				System.out.println("后缀式解析出错，退出");
+			}
+			System.out.println("后缀式：" + postFix);
+			try {
+				System.out.println("计算结果：" + PostfixStringHandler.handler(postFix));
+			} catch (Exception e) {
+				System.out.println("计算出错，退出");
+			}
+		}
+	}
 }
